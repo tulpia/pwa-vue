7613e4fdded54cc009ffd2697abe7f9476d850ce
tulpia <teihotaata.alec@gmail.com>
2019-05-23 13:47:05 +0200

    first commit

---
+++ precache-manifest.6f10165ef469d6e18a97f47aaf0038e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "428b6a8ee902925da866",
+    "url": "/pwa-alec/css/app.fb72fac6.css"
+  },
+  {
+    "revision": "428b6a8ee902925da866",
+    "url": "/pwa-alec/js/app.5d38bc68.js"
+  },
+  {
+    "revision": "d86e264c6d3242a781da",
+    "url": "/pwa-alec/css/chunk-vendors.19588e8d.css"
+  },
+  {
+    "revision": "d86e264c6d3242a781da",
+    "url": "/pwa-alec/js/chunk-vendors.ce07392b.js"
+  },
+  {
+    "revision": "2b8b78dc342fec59fbe5407f086a3ef2",
+    "url": "/pwa-alec/img/hotel.2b8b78dc.jpg"
+  },
+  {
+    "revision": "4a49fef7c58e990386560d587af406dc",
+    "url": "/pwa-alec/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa-alec/robots.txt"
+  }
+];
